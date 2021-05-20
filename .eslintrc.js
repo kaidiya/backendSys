f308b189e2e8fd16cf6dc40f2cc5da5013410bb2
linhan <linhan1@sfmail.sf-express.com>
2021-05-20 20:52:33 +0800

    modify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -15,10 +12,21 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        printWidth: 100,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        htmlWhitespaceSensitivity: 'ignore',
+        endOfLine: 'lf',
+      },
+    ],
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }], // 扩展名为’.tsx’的文件中不允许使用JSX，改为允许或者是直接关闭
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
   },
 };
--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>123123</div>,       document.getElementById('app'))
+ReactDOM.render(<div>123123</div>, document.getElementById('app'));
